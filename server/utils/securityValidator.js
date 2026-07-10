1c1bd48fe3e4b5ac62bd47ab5a7a0403dfac8f1b
Bhashit Maheshwari <bhashitm2@gmail.com>
2026-07-10 12:36:10 +0530

    Add development origin for Vercel deployment

--- server/utils/securityValidator.js
+++ server/utils/securityValidator.js
@@ -131,6 +131,7 @@ const developmentOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+  "https://otaku-world-nu.vercel.app"
 ];
 
 const normalizeOrigin = (origin) => {
